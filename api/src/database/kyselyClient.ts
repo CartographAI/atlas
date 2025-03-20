b612795f34cc801d56e671f0673b360bf2a672a0
Si-Yan Teo <dev@teosiyan.com>
2025-03-20 12:54:32 +0800

    feat(api): Set up kysely with postgres and type generation

---
+++ api/src/database/kyselyClient.ts
@@ -0,0 +1,25 @@
+import { Kysely, PostgresDialect } from "kysely";
+import * as pg from "pg";
+import type { DB } from "./types.d.ts";
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+if (!DATABASE_URL) {
+  throw new Error("Missing DATABASE_URL");
+}
+
+// Map int8 to number. This applies during runtime and does not affect the typescript types
+const int8TypeId = 20;
+pg.types.setTypeParser(int8TypeId, (val: string) => {
+  return Number.parseInt(val, 10);
+});
+
+const dbClient = new Kysely<DB>({
+  dialect: new PostgresDialect({
+    pool: new pg.Pool({
+      connectionString: DATABASE_URL,
+    }),
+  }),
+});
+
+export default dbClient;
